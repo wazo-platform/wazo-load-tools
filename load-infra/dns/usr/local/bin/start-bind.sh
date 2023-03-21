7b403bdb188fc6f20cf98c194c319ebef13d80e7
franckrupin <frupin@protonmail.com>
2023-03-21 06:59:44 -0400

    Fix typo

--- load-infra/dns/usr/local/bin/start-bind.sh
+++ load-infra/dns/usr/local/bin/start-bind.sh
@@ -5,6 +5,6 @@ docker run -d --rm --name=ddns-master \
     -v /opt/bind9/configuration/zones:/etc/bind/zones \
     --mount type=bind,source=/opt/bind9/configuration/named.conf,target=/etc/bind/named.conf \
     --mount type=bind,source=/opt/bind9/configuration/named.conf.default-zones,target=/etc/bind/named.conf.default-zones \
-    --mount type=bind,source=/opt/bind9/configuration/named.conf.local,target=/etc/bind/named.conf.local
-    --mount type=bind,source=/opt/bind9/configuration/named.conf.options,target=/etc/bind/named.conf.options
+    --mount type=bind,source=/opt/bind9/configuration/named.conf.local,target=/etc/bind/named.conf.local \
+    --mount type=bind,source=/opt/bind9/configuration/named.conf.options,target=/etc/bind/named.conf.options \
     ddns-master
