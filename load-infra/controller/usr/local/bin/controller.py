f250c929819a14b1fa355696e95321ce681d8201
franckrupin <frupin@protonmail.com>
2023-04-02 11:03:38 -0400

    Add coroutines for orchestarting the load

--- load-infra/controller/usr/local/bin/controller.py
+++ load-infra/controller/usr/local/bin/controller.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-import paramiko
-import scp
+import asyncio
 import yaml
+import queue
+import paramiko
+import sys
+
 
 class remoteControl():
     def __init__(self, host, scenario, script, container, keyfile, command):
@@ -41,14 +44,105 @@ class commandFactory():
         return f'docker exec -d {self.container} bash -c "export SCENARIO={self.scenario} /trafgen/load.sh" > /dev/null 2>&1 &'
 
 
+
+async def process_node(node, ttl, channel):
+    """
+    Coroutine that connects, processes end disconnect from the remote host. Connection duration 
+    is based on the TTL
+    """
+    host = node['host']
+    container = node['container']
+    #scenario = node['scenario']
+    #config = node.get('config')
+    #script = node.get('script')
+    #cmd = node.get('cmd')
+
+    msg = "started"
+    if container == "callee":
+        msg = await channel.get()
+    if msg != "started":
+        print(f"quitting due to receiving: {msg}")
+        return
+    # Connection to the remote host 
+    # ...
+
+    # Execute instruction to the remote host
+    # ...
+    if container == "caller":
+        await channel.put("started")
+        print("sent: started")
+
+
+    # Await on the TTL
+    await asyncio.sleep(ttl)
+
+    # Disconnect from the remote host
+    # ...
+    # test by printing
+    print(f"=============== {host}")
+
+async def process_load(load):
+    """
+    Coroutine that processes a work load by creating a process_node coroutine for each node.
+    """
+    nodes = load['load']
+    ttl = load['ttl']
+    coroutines = []
+
+    # create a queue that will allow coroutine to send status.
+    q = asyncio.Queue()
+
+    # Loop for creating a coroutine for each node in the workload.
+    for node in nodes:
+        coroutine = asyncio.create_task(process_node(node, ttl, q))
+        coroutines.append(coroutine)
+
+    # The coroutine is waiting for all coroutines terminate
+    await asyncio.gather(*coroutines)
+
+async def orchestrator(queue):
+    """
+    This coroutine consumes a queue containing the workload
+    """
+    while True:
+        # Waiting for a workload
+        load = queue.get()
+        print(load)
+
+        # Workload processing
+        await process_load(load)
+        queue.task_done()
+
+        # break when the queue is empty
+        if queue.empty():
+            break
+
+
 def parse_config(yml):
-    with open(yml, "r") as f:
-        trafgen = yaml.safe_load(f)
-    for load in trafgen["loads"]:
-        yield load
+    q = queue.Queue()
+    try:
+        with open(yml, "r") as f:
+            trafgen = yaml.safe_load(f)
+        for load in trafgen["loads"]:
+            q.put(load)
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit()
+
+    return q
+
+def main(loads_file):
+    q = parse_config(loads_file)
+    loop = asyncio.get_event_loop()
+
+    # call orchestrator with the work load
+    loop.run_until_complete(orchestrator(q))
+    loop.close()
 
 
 if __name__ == "__main__":
-    for load in  parse_config("trafgen.yml"):
-        for node in load["load"]:
-            print(node)
+    if len(sys.argv) > 1:
+        loads_file = sys.argv[1]
+        main(loads_file)
+    else:
+        main("trafgen.yml")
