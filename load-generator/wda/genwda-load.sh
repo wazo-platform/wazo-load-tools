536a4d7d617c22185fdc1951e5f06a1d601aff2a
franckrupin <frupin@protonmail.com>
2023-05-25 09:11:14 -0400

    Add config file and default values

--- load-generator/wda/genwda-load.sh
+++ load-generator/wda/genwda-load.sh
@@ -1,20 +1,63 @@
 #!/bin/bash
 
+source /etc/genwda-load.conf
+
+# define default values
+if [ -z $DEBUG ]; then
+    DEBUG=1
+fi
+if [ -z $DISABLE_CHATD ]; then
+    DISABLE_CHATD=1
+fi
+if [ -z $DURATION ]; then
+    DURATION=300
+fi
+if [ -z $TOKEN_EXPIRATION ]; then
+    TOKEN_EXPIRATION=600
+fi
+if [ -z $DELAY_CNX_RAND ]; then
+    DELAY_CNX_RAND=60
+fi
+if [ -z $TTL ]; then
+    TTL=30
+fi
+if [ -z $SERVER ]; then
+    SERVER=wazo-5000-1.load.wazo.io
+fi
+if [ -z $LOAD_SECTIONS ]; then
+    LOAD_SECTIONS=10
+fi
+if [ -z $CLIENTS ]; then
+    CLIENTS=10
+fi
+if [ -z $CONTAINERS ]; then
+    CONTAINERS=100
+fi
+if [ -z $REQUEST_TIMEOUT ]; then
+    REQUEST_TIMEOUT=300
+fi
+if [ -z $DISABLE_HEADER_CHECK ]; then
+    DISABLE_HEADER_CHECK=1
+fi
+if [ -z $TRAFGEN_NUMBER ]; then
+    TRAFGEN_NUMBER=10
+fi
+
+
+MAX_CLIENTS_PER_NODE=$(($LOAD_SECTIONS * $CLIENTS))
+if [ $MAX_CLIENTS_PER_NODE -gt $CONTAINERS ]; then
+    echo cannot run such ammount of clients on $CONTAINERS containers
+    exit 1
+fi
+
+
 START_USER=1000
 LOAD_FILE_NUM=0
 CONTAINER_NUM=0
 CONTAINER_TRACK=0
-DEBUG=1
-DISABLE_CHATD=1
-DURATION=300
-TOKEN_EXPIRATION=600
-DELAY_CNX_RAND=60
-TTL=30
 EXT="@wazo.io"
-#SERVER=router-1.load.wazo.io
-SERVER=wazo-5000-1.load.wazo.io
 
-LOAD_FILES_NUMBER=1
+#LOAD_FILES_NUMBER=1
 for x in $(seq 1 $LOAD_FILES_NUMBER);do # number of  load files
     LOAD_FILE_NUM=$((LOAD_FILE_NUM + 1))
     LOAD_FILE=wda$LOAD_FILE_NUM.yml
@@ -22,7 +65,7 @@ for x in $(seq 1 $LOAD_FILES_NUMBER);do # number of  load files
 cat >$LOAD_FILE <<EOF
 loads:
 EOF
-    LOAD_SECTIONS=5
+    #LOAD_SECTIONS=5
     for x in $(seq 1 $LOAD_SECTIONS); do  # number of load sections in the load file
         HOST_NUM=0
     echo LOAD SECTION
@@ -30,13 +73,13 @@ cat >>$LOAD_FILE <<EOF
   - load:
 EOF
 
-        TRAFGEN_NUMBER=10
+        #TRAFGEN_NUMBER=10
         for x in $(seq 1 $TRAFGEN_NUMBER); do  # trafgen number
             HOST_NUM=$(( HOST_NUM + 1 ))
             HOST=trafgen$HOST_NUM.load.wazo.io
             CONTAINER_NUM=$CONTAINER_TRACK
 
-            CLIENTS=20
+            #CLIENTS=20
             for y in $(seq 1 $CLIENTS); do  # number of clients
                 CONTAINER_NUM=$(( CONTAINER_NUM + 1 ))
                 START_USER=$(( START_USER + 1 ))
@@ -54,6 +97,8 @@ cat >>$LOAD_FILE <<EOF
         DEBUG: $DEBUG
         TOKEN_EXPIRATION: $TOKEN_EXPIRATION
         DISABLE_CHATD: $DISABLE_CHATD
+        DISABLE_HEADER_CHECK: $DISABLE_HEADER_CHECK
+        REQUEST_TIMEOUT: $REQUEST_TIMEOUT
 EOF
             done
 
@@ -69,4 +114,4 @@ EOF
 
 done
 
-scp wda*.yml grafana:/root/load/without-router
+#scp wda*.yml grafana:/root/load/without-router
