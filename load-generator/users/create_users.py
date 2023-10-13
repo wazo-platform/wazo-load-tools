559a03284deb777bdcc8ddad54becb28c6b0ef23
Pascal Cadotte Michaud <pcm@wazo.io>
2023-10-13 09:10:52 -0400

    make user creation concurrent

--- load-generator/users/create_users.py
+++ load-generator/users/create_users.py
@@ -2,8 +2,22 @@
 # Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import multiprocessing
+import concurrent.futures
 import subprocess
 import sys
+from functools import partial
+
+
+def run(lock, parameter_file, n):
+    cmd = ['./usergen.sh', str(n), parameter_file]
+    print('executing', cmd)
+    output = subprocess.run(cmd, shell=False, stdout=subprocess.PIPE).stdout
+    print(output)
+
+    with lock:
+        with open('output.txt', 'a') as f:
+            f.writelines(output)
 
 
 def main():
@@ -11,10 +25,9 @@ def main():
     count = int(sys.argv[2])
     parameter_file = sys.argv[3]
 
-    for n in range(start, start+count):
-        cmd = ['./usergen.sh', str(n), parameter_file]
-        proc = subprocess.Popen(cmd, shell=False)
-        proc.communicate()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        lock = multiprocessing.Manager().Lock()
+        executor.map(partial(run, lock, parameter_file), range(start, start+count))
 
 
 if __name__ == '__main__':
