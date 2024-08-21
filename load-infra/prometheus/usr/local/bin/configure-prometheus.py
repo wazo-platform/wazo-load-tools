231b032c0b1d9311ac73478e362561122e3c8846
Pascal Cadotte Michaud <pcm@wazo.io>
2024-08-21 11:14:00 -0400

    prometheus: generate the config file

---
+++ load-infra/prometheus/usr/local/bin/configure-prometheus.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import yaml
+import click
+
+from jinja2 import Environment, FileSystemLoader
+
+@click.command()
+@click.option(
+    '--config',
+    help='Configuration file for the config generator',
+    required=True,
+)
+@click.option(
+    '--template-dir',
+    default='/etc/prometheus/templates',
+    help='Directory where the templates live',
+)
+@click.option(
+    '--template-name',
+    default='prometheus.yml.jinja2',
+    help='Name of the template to render',
+)
+def main(config, template_dir, template_name):
+    with open(config) as f:
+        conf = yaml.safe_load(f)
+    environment = Environment(
+        loader=FileSystemLoader([template_dir]),
+    )
+    template = environment.get_template(template_name)
+    print(template.render(
+        stacks=conf['wazo-stack'],
+        edges=conf['wazo-edge'],
+    ))
+
+
+if __name__ == '__main__':
+    main()
